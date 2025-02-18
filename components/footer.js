72b1ffd2280e297375bdb9e40a76124ca42f7f86
OLUWANIYIAKIN <olu24002@byui.edu>
2025-02-18 02:36:08 +0100

    hh

--- components/footer.js
+++
@@ -1,9 +0,0 @@
-const createFooter = () => {
-    const footer = document.createElement('footer');
-    footer.innerHTML = `
-        <p>Healthy Meal Planner &copy; 2025</p>
-    `;
-    document.body.appendChild(footer);
-};
-
-export { createFooter };
